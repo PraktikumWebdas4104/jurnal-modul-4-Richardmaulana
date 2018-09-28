66dc97419bf271869fab19b6bdea22b91f4eed28
MUCHAMMAD RICHARD MAULANA <43516487+Richardmaulana@users.noreply.github.com>
2018-09-28 09:44:58 +0700

    Update formawal.php
    Muchammad Richard Maulana
    6701174072

--- formawal.php
+++ formawal.php
@@ -1,5 +1,15 @@
-<?php
-
-
-
-?>
+<form method="post" action="proses.php">
+	<table>
+		<TR>
+			<td>Username</td>
+			<td>:</td>
+			<td><input type="text" name="username"></td>
+		</TR>
+		<TR>
+			<td>Password</td>
+			<td>:</td>
+			<td><input type="password" name="password"></td>
+		</TR>
+	</table>
+	<input type="submit" name="submit" value="Klik mas">
+</form>
